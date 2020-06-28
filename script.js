1c4cbd3e3bc115768ab8080ccff6dfdc01f7edc7
Ruben80 <43403734+Ruben80@users.noreply.github.com>
2020-06-28 14:24:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/* let multi = 1230941 * 1823794; // Dit is een grote som
+console.log(multi);
+
+let dev = 637263 / 54;
+console.log(dev); */
+
+console.log("Hallo Winc Academy studenten");
+
+/* Hoe zal het toch komen dat je deze comments niet zien? */
